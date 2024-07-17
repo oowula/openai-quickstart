00366efa05ebcebc115b4d9a097e722deeb290ee
Laughing Wu <laughing.wu@oocl.com>
2024-07-17 11:33:24 +0800

    day2 - openai_translator homework: modify prompt

--- openai-translator/ai_translator/model/model.py
+++ openai-translator/ai_translator/model/model.py
@@ -2,11 +2,11 @@ from book import ContentType
 
 class Model:
     def make_text_prompt(self, text: str, target_language: str) -> str:
-        return f"翻译为{target_language}：{text}"
+        return f"把下面第一个：后面的所有文本翻译为{target_language}，只允许输出翻译的结果：\n{text}"
 
     def make_table_prompt(self, table: str, target_language: str) -> str:
         # return f"翻译为{target_language}，保持间距（空格，分隔符），以表格形式返回：\n{table}"
-        return f"翻译为{target_language}，以空格和换行符表示表格：\n{table}"
+        return f"把下面第一个：后面的所有文本翻译为{target_language}。只允许按照格式输出翻译的结果，以空格和换行符表示输出表格的格式。列与列之间会使用空格进行分割，必须确保输出中的列数相同，必要时可以使用-进行填充：\n{table}"
 
     def translate_prompt(self, content, target_language: str) -> str:
         if content.content_type == ContentType.TEXT:
